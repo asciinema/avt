c29b309c44e83c0c462f65940c683b6a4592436b
Marcin Kulik <m@ku1ik.com>
2023-08-28 16:23:21 +0200

    Add chars() and text() methods to Line

--- src/line.rs
+++ src/line.rs
@@ -50,6 +50,14 @@ impl Line {
             segment: None,
         }
     }
+
+    pub fn chars(&self) -> impl Iterator<Item = char> + '_ {
+        self.0.iter().map(|cell| cell.0)
+    }
+
+    pub fn text(&self) -> String {
+        self.chars().collect()
+    }
 }
 
 struct Chunk<'a, I>
