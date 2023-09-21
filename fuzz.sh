a465c07c41fd35d37107670b862107cc303d69b7
Marcin Kulik <m@ku1ik.com>
2023-09-21 12:22:06 +0200

    Support passing test name to fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+test=${1:-prop_}
+
 while true
 do
-    PROPTEST_CASES=100000 RUST_BACKTRACE=1 cargo test prop_
+    PROPTEST_CASES=100000 RUST_BACKTRACE=1 cargo test $test
 
     if [[ x$? != x0 ]] ; then
         exit $?
