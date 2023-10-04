859392999505530b1adcda9eef9dfb8581a30c76
Marcin Kulik <m@ku1ik.com>
2023-10-04 16:06:04 +0200

    Add bench for feeding in small chunks

--- benches/feed.rs
+++ benches/feed.rs
@@ -15,13 +15,39 @@ fn run_bulk((mut vt, text): (Vt, String)) -> (Vt, String) {
     (vt, text)
 }
 
+fn setup_chunks() -> (Vt, Vec<String>) {
+    let vt = Vt::with_scrollback_limit(100, 24, Some(1000));
+    let text = sample_text();
+
+    let chunks: Vec<String> = text
+        .chars()
+        .collect::<Vec<char>>()
+        .chunks(32)
+        .map(String::from_iter)
+        .collect();
+
+    (vt, chunks)
+}
+
+fn run_chunks((mut vt, chunks): (Vt, Vec<String>)) -> (Vt, Vec<String>) {
+    for text in &chunks {
+        vt.feed_str(text);
+    }
+
+    (vt, chunks)
+}
+
 fn sample_text() -> String {
     fs::read_to_string("benches/sample.txt").unwrap()
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("feed", |b| {
+    c.bench_function("feed in bulk", |b| {
         b.iter_batched(setup_bulk, run_bulk, BatchSize::SmallInput)
+    });
+
+    c.bench_function("feed in chunks", |b| {
+        b.iter_batched(setup_chunks, run_chunks, BatchSize::SmallInput)
     });
 }
 
