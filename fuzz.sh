769f5804c09dc8a4149a5cda953383b222387aa0
Marcin Kulik <m@ku1ik.com>
2023-10-11 11:32:14 +0200

    Increase fuzz iterations

--- fuzz.sh
+++ fuzz.sh
@@ -6,7 +6,7 @@ export CARGO_PROFILE_TEST_OPT_LEVEL=3
 
 while true
 do
-    PROPTEST_CASES=100000 RUST_BACKTRACE=1 cargo test $test
+    PROPTEST_CASES=1000000 RUST_BACKTRACE=1 cargo test $test
 
     if [[ x$? != x0 ]] ; then
         exit $?
