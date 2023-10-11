8ed0247266471128806839f774f96db2aeb92b6f
Marcin Kulik <m@ku1ik.com>
2023-10-11 11:27:55 +0200

    Refactor benchmarks

--- benches/parser.rs
+++ benches/parser.rs
@@ -2,16 +2,38 @@ use avt::parser::{Executor, Parser};
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use std::fs;
 
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("mixed", |b| {
+        b.iter_batched(setup("mixed.txt"), run, BatchSize::SmallInput)
+    });
+
+    c.bench_function("cacademo", |b| {
+        b.iter_batched(setup("cacademo.txt"), run, BatchSize::SmallInput)
+    });
+
+    c.bench_function("licenses", |b| {
+        b.iter_batched(setup("licenses.txt"), run, BatchSize::SmallInput)
+    });
+
+    c.bench_function("licenses-lolcat", |b| {
+        b.iter_batched(setup("licenses-lolcat.txt"), run, BatchSize::SmallInput)
+    });
+}
+
 struct NoopExecutor();
 
 impl Executor for NoopExecutor {}
 
-fn setup() -> (Parser, String, NoopExecutor) {
-    let parser = Parser::default();
-    let text = sample_text();
-    let executor = NoopExecutor {};
+fn setup(filename: &str) -> impl Fn() -> (Parser, String, NoopExecutor) {
+    let filename = filename.to_owned();
 
-    (parser, text, executor)
+    move || {
+        let parser = Parser::default();
+        let text = sample_text(&filename);
+        let executor = NoopExecutor {};
+
+        (parser, text, executor)
+    }
 }
 
 fn run<E: Executor>((mut parser, text, mut executor): (Parser, String, E)) -> (Parser, String, E) {
@@ -20,14 +42,8 @@ fn run<E: Executor>((mut parser, text, mut executor): (Parser, String, E)) -> (P
     (parser, text, executor)
 }
 
-fn sample_text() -> String {
-    fs::read_to_string("benches/sample.txt").unwrap()
-}
-
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("feed_str", |b| {
-        b.iter_batched(setup, run, BatchSize::SmallInput)
-    });
+fn sample_text(filename: &str) -> String {
+    fs::read_to_string(format!("benches/data/{filename}")).unwrap()
 }
 
 criterion_group!(benches, criterion_benchmark);
---
+++ benches/vt.rs
@@ -0,0 +1,118 @@
+use avt::Vt;
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use std::fs;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("feed mixed in bulk", |b| {
+        b.iter_batched(setup_feed("mixed.txt"), run_feed, BatchSize::SmallInput)
+    });
+
+    c.bench_function("feed mixed in chunks", |b| {
+        b.iter_batched(
+            chunk(setup_feed("mixed.txt")),
+            run_feed,
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("feed cacademo in bulk", |b| {
+        b.iter_batched(setup_feed("cacademo.txt"), run_feed, BatchSize::SmallInput)
+    });
+
+    c.bench_function("feed cacademo in chunks", |b| {
+        b.iter_batched(
+            chunk(setup_feed("cacademo.txt")),
+            run_feed,
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("feed licenses in bulk", |b| {
+        b.iter_batched(setup_feed("licenses.txt"), run_feed, BatchSize::SmallInput)
+    });
+
+    c.bench_function("feed licenses in chunks", |b| {
+        b.iter_batched(
+            chunk(setup_feed("licenses.txt")),
+            run_feed,
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("feed licenses-lolcat in bulk", |b| {
+        b.iter_batched(
+            setup_feed("licenses-lolcat.txt"),
+            run_feed,
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("feed licenses-lolcat in chunks", |b| {
+        b.iter_batched(
+            chunk(setup_feed("licenses-lolcat.txt")),
+            run_feed,
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("dump", |b| {
+        b.iter_batched(setup_dump, run_dump, BatchSize::SmallInput)
+    });
+}
+
+fn setup_feed(filename: &str) -> impl Fn() -> (Vt, Vec<String>) {
+    let filename = filename.to_owned();
+
+    move || {
+        let vt = Vt::with_scrollback_limit(100, 24, Some(1000));
+        let text = sample_text(&filename);
+
+        (vt, vec![text])
+    }
+}
+
+fn run_feed((mut vt, chunks): (Vt, Vec<String>)) -> (Vt, Vec<String>) {
+    for text in &chunks {
+        vt.feed_str(text);
+    }
+
+    (vt, chunks)
+}
+
+fn chunk(f: impl Fn() -> (Vt, Vec<String>)) -> impl Fn() -> (Vt, Vec<String>) {
+    move || {
+        let (vt, text) = f();
+
+        let chunks: Vec<String> = text
+            .first()
+            .unwrap()
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(32)
+            .map(String::from_iter)
+            .collect();
+
+        (vt, chunks)
+    }
+}
+
+fn sample_text(filename: &str) -> String {
+    fs::read_to_string(format!("benches/data/{filename}")).unwrap()
+}
+
+fn setup_dump() -> Vt {
+    let mut vt = Vt::new(100, 40);
+    let text = &sample_text("mixed.txt")[0..128 * 1024];
+    vt.feed_str(text);
+
+    vt
+}
+
+fn run_dump(vt: Vt) -> Vt {
+    vt.dump();
+
+    vt
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
