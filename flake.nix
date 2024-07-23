3d66225c0ded1c58b861a8d241358acddf67823e
Marcin Kulik <m@ku1ik.com>
2024-07-23 22:18:44 +0200

    Use oxalica/rust-overlay in dev shell flake

--- flake.nix
+++ flake.nix
@@ -2,20 +2,30 @@
   description = "asciinema virtual terminal";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = pkgs.mkShell {
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    let
+      packageToml =
+        (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+      msrv = packageToml.rust-version;
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        mkDevShell = rust:
+          pkgs.mkShell {
             nativeBuildInputs = [
-              pkgs.rustup
+              (rust.override { extensions = [ "rust-src" "rust-analyzer" ]; })
             ];
           };
-        }
-      );
+      in {
+        devShells.default = mkDevShell pkgs.rust-bin.stable.latest.default;
+        devShells.msrv = mkDevShell pkgs.rust-bin.stable.${msrv}.default;
+      });
 }
